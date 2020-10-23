ec438993f9d84f43f0f2da664bac89af831aca56
arameshraju <alluri.ramesh@gmail.com>
2020-10-23 20:53:35 +0530

    5 files added

---
+++ components/Despatch.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Despatch extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h2>Despatch</h2>
+      </React.Fragment>
+    );
+  }
+}
+export default Despatch;
---
+++ components/PatientMaster.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class PatinetMaster extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h2>PatinetMaster</h2>
+      </React.Fragment>
+    );
+  }
+}
+export default PatinetMaster;
