8648477e779e89b640cacf234d77e6f3ec683dad
arameshraju <alluri.ramesh@gmail.com>
2020-10-24 19:03:34 +0530

    3 files modified

--- components/PatientMaster.js
+++ components/PatientMaster.js
@@ -8,52 +8,54 @@ class PatinetMaster extends Component {
   render() {
     return (
       <React.Fragment>
-        <h2>Patinet Master </h2>
-        <form>
-          <table>
-            <tbody>
-              <tr>
-                <td> Patinet Name</td>
-                <td>
-                  <input type="text" name="name" />
-                </td>
-              </tr>
-              <tr>
-                <td> MobileNo </td>
-                <td>
-                  <input type="text" name="MobileNo" />
-                </td>
-              </tr>
-              <tr>
-                <td> Village/Town </td>
-                <td>
-                  <input type="text" name="Village" />
-                </td>
-              </tr>
-              <tr>
-                <td> Reference </td>
-                <td>
-                  <input type="text" name="Reference" />
-                </td>
-              </tr>
-              <tr>
-                <td> OtherInfo </td>
-                <td>
-                  <input type="text" name="OtherInfo" />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  {" "}
-                  <input type="Reset" value="Clear" />{" "}
-                </td>
-                <td>
-                  <input type="submit" value="Save" />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </form>
+        <div class="col-3 align-self-center">
+          <h2>Patinet Master </h2>
+          <form>
+            <table class="table">
+              <tbody>
+                <tr>
+                  <td> Patinet Name</td>
+                  <td>
+                    <input type="text" name="name" />
+                  </td>
+                </tr>
+                <tr>
+                  <td> MobileNo </td>
+                  <td>
+                    <input type="text" name="MobileNo" />
+                  </td>
+                </tr>
+                <tr>
+                  <td> Village/Town </td>
+                  <td>
+                    <input type="text" name="Village" />
+                  </td>
+                </tr>
+                <tr>
+                  <td> Reference </td>
+                  <td>
+                    <input type="text" name="Reference" />
+                  </td>
+                </tr>
+                <tr>
+                  <td> OtherInfo </td>
+                  <td>
+                    <input type="text" name="OtherInfo" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    {" "}
+                    <input type="Reset" value="Clear" />{" "}
+                  </td>
+                  <td>
+                    <input type="submit" value="Save" />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </form>
+        </div>
       </React.Fragment>
     );
   }
--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Home from './home';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Home from "./home";
+import "./style.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'Care Manager'
+      name: "Care Manager"
     };
   }
 
   render() {
     return (
-      <div>
-        <Home/>
+      <div class="row ">
+        <Home />
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
