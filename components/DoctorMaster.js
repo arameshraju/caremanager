ef62fe2fed844eff4754d220bd07d0093a712689
arameshraju <alluri.ramesh@gmail.com>
2020-10-24 18:31:18 +0530

    3 files modified, 1 added

--- components/DoctorMaster.js
+++ components/DoctorMaster.js
@@ -11,51 +11,53 @@ class DoctorMaster extends Component {
         <h2>Doctor Master </h2>
         <form>
           <table>
-            <tr>
-              <td> Doctor Name</td>
-              <td>
-                <input type="text" name="name" />
-              </td>
-            </tr>
-            <tr>
-              <td> Qualification </td>
-              <td>
-                <input type="text" name="Qualification" />
-              </td>
-            </tr>
-            <tr>
-              <td> MobileNo </td>
-              <td>
-                <input type="text" name="MobileNo" />
-              </td>
-            </tr>
-            <tr>
-              <td> RegNo </td>
-              <td>
-                <input type="text" name="RegNo" />
-              </td>
-            </tr>
-            <tr>
-              <td> email </td>
-              <td>
-                <input type="text" name="email" />
-              </td>
-            </tr>
-            <tr>
-              <td> Address </td>
-              <td>
-                <input type="text" name="Address" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                {" "}
-                <input type="Reset" value="Clear" />{" "}
-              </td>
-              <td>
-                <input type="submit" value="Save" />
-              </td>
-            </tr>
+            <tbody>
+              <tr>
+                <td> Doctor Name</td>
+                <td>
+                  <input type="text" name="name" />
+                </td>
+              </tr>
+              <tr>
+                <td> Qualification </td>
+                <td>
+                  <input type="text" name="Qualification" />
+                </td>
+              </tr>
+              <tr>
+                <td> MobileNo </td>
+                <td>
+                  <input type="text" name="MobileNo" />
+                </td>
+              </tr>
+              <tr>
+                <td> RegNo </td>
+                <td>
+                  <input type="text" name="RegNo" />
+                </td>
+              </tr>
+              <tr>
+                <td> email </td>
+                <td>
+                  <input type="text" name="email" />
+                </td>
+              </tr>
+              <tr>
+                <td> Address </td>
+                <td>
+                  <input type="text" name="Address" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  {" "}
+                  <input type="Reset" value="Clear" />{" "}
+                </td>
+                <td>
+                  <input type="submit" value="Save" />
+                </td>
+              </tr>
+            </tbody>
           </table>
         </form>
       </React.Fragment>
--- components/PatientMaster.js
+++ components/PatientMaster.js
@@ -11,45 +11,47 @@ class PatinetMaster extends Component {
         <h2>Patinet Master </h2>
         <form>
           <table>
-            <tr>
-              <td> Patinet Name</td>
-              <td>
-                <input type="text" name="name" />
-              </td>
-            </tr>
-            <tr>
-              <td> MobileNo </td>
-              <td>
-                <input type="text" name="MobileNo" />
-              </td>
-            </tr>
-            <tr>
-              <td> Village/Town </td>
-              <td>
-                <input type="text" name="Village" />
-              </td>
-            </tr>
-            <tr>
-              <td> Reference </td>
-              <td>
-                <input type="text" name="Reference" />
-              </td>
-            </tr>
-            <tr>
-              <td> OtherInfo </td>
-              <td>
-                <input type="text" name="OtherInfo" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                {" "}
-                <input type="Reset" value="Clear" />{" "}
-              </td>
-              <td>
-                <input type="submit" value="Save" />
-              </td>
-            </tr>
+            <tbody>
+              <tr>
+                <td> Patinet Name</td>
+                <td>
+                  <input type="text" name="name" />
+                </td>
+              </tr>
+              <tr>
+                <td> MobileNo </td>
+                <td>
+                  <input type="text" name="MobileNo" />
+                </td>
+              </tr>
+              <tr>
+                <td> Village/Town </td>
+                <td>
+                  <input type="text" name="Village" />
+                </td>
+              </tr>
+              <tr>
+                <td> Reference </td>
+                <td>
+                  <input type="text" name="Reference" />
+                </td>
+              </tr>
+              <tr>
+                <td> OtherInfo </td>
+                <td>
+                  <input type="text" name="OtherInfo" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  {" "}
+                  <input type="Reset" value="Clear" />{" "}
+                </td>
+                <td>
+                  <input type="submit" value="Save" />
+                </td>
+              </tr>
+            </tbody>
           </table>
         </form>
       </React.Fragment>
