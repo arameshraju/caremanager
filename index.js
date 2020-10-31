c5991d00b958bd5d1b2b4bfcd8fffcb0b55ac0a1
arameshraju <alluri.ramesh@gmail.com>
2020-10-31 19:01:05 +0530

    css

--- index.js
+++ index.js
@@ -16,20 +16,39 @@ export default function App() {
   return (
     <Router>
       <div>
-        <ul>
-          <li>
-            <Link to="/">Login</Link>
-          </li>
-          <li>
-            <Link to="/DashBoard">DashBoard</Link>
-          </li>
-          <li>
-            <Link to="/About">About</Link>
-          </li>
-          <li>
-            <Link to="/Support">Support</Link>
-          </li>
-        </ul>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+          <a class="navbar-brand" href="#">
+            DoCare
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon" />
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto ">
+              <li class="nav-item active  ">
+                <Link to="/">Login</Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/DashBoard">DashBoard</Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/About">About</Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/Support">Support</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
 
         <hr />
 
