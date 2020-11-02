50e77d3d3e1371ac68c2cdf6fe8fad67b112b5ee
arameshraju <alluri.ramesh@gmail.com>
2020-11-02 22:14:26 +0530

    Dashboard Menu

--- components/DashBoard.js
+++ components/DashBoard.js
@@ -4,6 +4,26 @@ function DashBoard() {
   return (
     <React.Fragment>
       <h2>DashBoard</h2>
+      <div className="row">
+        <div className="col-3">
+          <Link to="/PatientMaster">Patient</Link>
+        </div>
+        <div className="col-2">
+          <Link to="/DoctorMaster">Doctor</Link>
+        </div>
+        <div className="col-3">
+          <Link to="/Appointment">Appointment</Link>
+        </div>
+        <div className="col-2">
+          <Link to="/Prescribe">Prescribe</Link>
+        </div>
+        <div className="col-2">
+          <Link to="/Despatch">Despatch</Link>
+        </div>
+        <div className="col-sm-2">
+          <Link to="/DaysReport">Reports</Link>
+        </div>
+      </div>
       <ul>
         <li>
           <Link to="/PatientMaster">PatientMaster</Link>
