acdafb4c8ff93e964a3a9b039b1979263df3a24c
arameshraju <alluri.ramesh@gmail.com>
2020-11-03 11:09:45 +0530

    ids updated

--- service/Patient.js
+++ service/Patient.js
@@ -2,31 +2,37 @@ import React from "react";
 class Patient {
   patientData = [
     {
+      pid:1,
       name: "Ramesh",
       mobile: "989898989898",
       village: "Hyderabad"
     },
     {
+      pid:2,
       name: "Pavan",
       mobile: "9797979797",
       village: "Bhimavaram"
     },
     {
+      pid:2,
       name: "Pavan",
       mobile: "9797979797",
       village: "Bhimavaram"
     },
     {
+      pid:4,
       name: "Gupta",
       mobile: "9393939393",
       village: "Bhimavaram"
     },
     {
+      pid:5,
       name: "Prasad",
       mobile: "9292929292",
       village: "Kaikaluru"
     },
     {
+      pid:6,
       name: "Venkat",
       mobile: "9191919191",
       village: "USA"
