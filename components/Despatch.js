589c326a85e12431995a3b10d13a380dc0c62d37
arameshraju <alluri.ramesh@gmail.com>
2020-11-14 21:38:17 +0530

    Dispatch Wait List

--- components/Despatch.js
+++ components/Despatch.js
@@ -1,14 +1,54 @@
 import React, { Component } from "react";
-
+import Prescribe from "./Prescribe ";
+import TransData from "./../service/TransData";
 class Despatch extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      appData: {}
+    };
+    this.transData = new TransData();
   }
+  openAppointment = data => {
+    console.log("Appointment " + data.name);
+    this.setState({ appData: data });
+  };
 
   render() {
     return (
       <React.Fragment>
-        <h2>Despatch</h2>
+        <div className="row">
+          <h2>Consultation Details</h2>
+          <Prescribe appData={this.state.appData} />
+        </div>
+        <h2>Consultation Wait list</h2>
+        <div className="row">
+          <table className="table">
+            <thead>
+              <td>Name</td>
+              <td>doctor</td>
+              <td>symptom</td>
+              <td>Action</td>
+            </thead>
+            <tbody>
+              {this.transData
+                .SearchData()
+                .filter(tdata => tdata.status == "P")
+                .map(item => (
+                  <tr>
+                    <td>{item.name}</td>
+                    <td>{item.docname}</td>
+                    <td>{item.symptom}</td>
+                    <td>
+                      <button onClick={() => this.openAppointment(item)}>
+                        Paid
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
       </React.Fragment>
     );
   }
